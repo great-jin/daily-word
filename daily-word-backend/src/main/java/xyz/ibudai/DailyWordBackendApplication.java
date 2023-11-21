49e87874240bdeaf075abd6d93c319d0c9e15c64
great <great__jin@163.com>
2023-11-21 22:14:51 +0800

    init: Initial project

---
+++ daily-word-backend/src/main/java/xyz/ibudai/DailyWordBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.ibudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyWordBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyWordBackendApplication.class, args);
+    }
+
+}
